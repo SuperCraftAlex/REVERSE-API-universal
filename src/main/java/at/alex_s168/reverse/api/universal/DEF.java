ef8af3fdc7c5e1d1412f37410208840d07ac3e1d
SuperCraftAlex <63254202+SuperCraftAlex@users.noreply.github.com>
2023-05-05 23:02:27 +0200

    Update DEF.java

--- src/main/java/at/alex_s168/reverse/api/universal/DEF.java
+++ src/main/java/at/alex_s168/reverse/api/universal/DEF.java
@@ -3,6 +3,7 @@ package at.alex_s168.reverse.api.universal;
 public class DEF {
 
     public static int PROTOCOL_VERSION = 1;
+    public static int PORT = 1154;
 
     public static int STRING_LENGTH = 2_000_000_000;
     public static int USERNAME_LENGTH = 18;
